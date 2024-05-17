55f6b4082e68f2793d68f6be8ac5ab027eda8fb1
rekomo <rekomo.code@protonmail.com>
2024-05-17 16:57:11 +0200

    fix assimp dll not included

--- init.bat
+++ init.bat
@@ -1,4 +1,13 @@
 @echo off
+
+IF "%1"=="" (
+    SET arg1=vs2022
+	echo Generating solution for default: vs2022
+) ELSE (
+    SET arg1=%1
+)
+
 git submodule init
 git submodule update
-.\vendor\premake5\premake5.exe %1
+
+.\vendor\premake5\premake5.exe %arg1%
